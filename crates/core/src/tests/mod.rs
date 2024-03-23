5dff962898b9e9d441ce26131834877113504c80
Kazik24 <kazik24official@gmail.com>
2024-03-23 11:41:32 +0100

    Assert chungus tests after 1000 ticks

--- crates/core/src/tests/mod.rs
+++ crates/core/src/tests/mod.rs
@@ -4,6 +4,7 @@ use mchprs_blocks::blocks::{Block, ButtonFace};
 use mchprs_blocks::{BlockFace, BlockPos};
 use mchprs_save_data::plot_data::PlotData;
 use mchprs_world::TickPriority;
+use sha2::{Digest, Sha256};
 
 use crate::plot::{PlotWorld, PLOT_WIDTH};
 use crate::redstone;
@@ -54,7 +55,22 @@ fn run_mandelbrot_chungus() {
 
     click_floor_button(&mut plot, CHUNGUS_START_BUTTON);
 
-    for _ in 0..100 {
+    for _ in 0..1000 {
         plot.tick_interpreted();
     }
+
+    let mut hasher = Sha256::new();
+    for chunk in &plot.chunks {
+        for x in 0..16 {
+            for z in 0..16 {
+                for y in 0..256 {
+                    let ch = chunk.get_block(x, y, z);
+                    hasher.update(ch.to_le_bytes());
+                }
+            }
+        }
+    }
+    let hash = hasher.finalize();
+    //hash after 1000 ticks
+    assert_eq!(hash.as_slice(),b"\xaf\xe1\xb6\xf2\xe9\xfa\xe4\x5b\xa9\x68\xc1\x0a\x6e\x4b\xf7\xb0\x29\x78\xc5\xb3\x9c\xc3\xec\xb4\xe0\x73\x0a\xf3\x8e\x94\x20\x05");
 }
